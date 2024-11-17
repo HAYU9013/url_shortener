42ced96f2cef4796b26c56a4219aadb546b28f87
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-11-17 21:29:28 +0800

    add todo route

--- routes/router.go
+++ routes/router.go
@@ -1,18 +1,16 @@
 package routes
 
 import (
-	"url_shortener/controllers"
-
+	// TODO: import url_shortener/controllers package
 	"github.com/gin-gonic/gin"
 )
 
 // SetupRoutes 設置應用程式的路由
 // r *gin.Engine 是 Gin 框架的引擎實例
-// r.Static("/public", "./public") 設置靜態文件的路徑
-// r.POST("/shorten", controllers.ShortenURL) 設置縮短 URL 的 POST 路由
-// r.GET("/r/:code", controllers.RedirectURL) 設置重定向 URL 的 GET 路由
 func SetupRoutes(r *gin.Engine) {
 	r.Static("/public", "./public")
-	r.POST("/shorten", controllers.ShortenURL)
-	r.GET("/r/:code", controllers.RedirectURL)
+	// TODO: set POST method of /shorten for shorten url, call controllers.ShortenURL
+
+	// TODO: set GET method of /r/:code for redirect url, call controllers.RedirectURL
+
 }
